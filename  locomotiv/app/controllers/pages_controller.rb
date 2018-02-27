8ca60e2a4b6312e01e5a023f44db07273b59d600
Samy Chalakani <samychalakani@gmail.com>
2018-02-27 19:31:57 +0100

    start

---  locomotiv/app/controllers/pages_controller.rb
+++  locomotiv/app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :accompagner]
 
   def home
+    @projects= Project.all
   end
+
+
+  def accompagner
+  end
+
+private
+  def project_params
+  params.require(:project).permit(:name, :domain)
+end
+
 end
---  locomotiv/config/routes.rb
+++  locomotiv/config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  get 'projects/new'
+
+  get 'projects/create'
+
+  get 'projects/edit'
+
+  get 'projects/destroy'
+
+  get 'projects/index'
+
+  get 'projects/update'
+
+  get 'projects/show'
+
+get 'accompagner' => 'pages#accompagner'
+
+
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
